e8088f175e1c5e83f47bac2e49d4b3f6cbcc8e0e
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-23 10:22:56 +0200

    add jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
   return (
     <div className='App'>
       <h1>Jobs</h1>
+      <p>There are {jobs.length} jobs!</p>
     </div>
   );
 }
