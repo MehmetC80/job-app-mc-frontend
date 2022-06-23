2ce0fe79c8791efe30bd10ffcca40b370077070e
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-23 11:51:46 +0200

    loading ux

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,20 @@ function App() {
   return (
     <div className='App'>
       <h1>Jobs</h1>
-      <p>There are {myJobs.length} jobs!</p>
-      <ul>
-        {myJobs.jobs.map((job) => {
-          return <li>{job.position}</li>;
-        })}
-      </ul>
+
+      {myJobs.jobs.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {' '}
+          <p>There are {myJobs.length} jobs!</p>
+          <ul>
+            {myJobs.jobs.map((job) => {
+              return <li>{job.position}</li>;
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
