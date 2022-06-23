8e80b4b9b933fa1a4aa851e8649d5568185cdfe9
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-06-23 11:06:58 +0200

    setup env file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
 
+// const backendUrl = 'http://localhost:4711/jobs';
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const url = `${backendUrl}/jobs`;
+
 function App() {
   const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setJobs(data);
+    };
+    fetchJobs();
+  });
 
   return (
     <div className='App'>
